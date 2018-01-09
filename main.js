3d3ec4587400eee32ecd046cdd6a7de5a213cf7f
EdwardGoomba <ask@edwardvdesign.com>
2018-01-09 18:13:05 -0500

    Refactor request to be async

--- main.js
+++ main.js
@@ -9,53 +9,32 @@ const $shortenButton = $('#shorten');
 const $responseField = $('#responseField');
 
 // AJAX functions
-function expandUrl() {
-	const urlToExpand = url + '?shortUrl=' + $inputField.val() + '&key=' + apiKey;
-	fetch(urlToExpand).then(response => {
-		if (response.ok) {
-			return response.json();
-		}
-		throw new Error('Request failed!');
-	}, networkError => {
-		console.log(networkError.message);
-	}).then(jsonResponse => {
-    $responseField.append('<p> Your expanded URL is </p><p> ' + jsonResponse.longUrl + '</p>');
-return jsonResponse;
-  });
-};
-
-function shortenUrl() {
-  const urlWithKey = url + '?key=' + apiKey;
-	const urlToShorten = $inputField.val();
-
-  fetch(urlWithKey, {
-    method: 'POST',
-    headers: {
- "Content-type": "application/json"
-},
-    body: JSON.stringify({longUrl: urlToShorten})
-  }).then((response) => {
+async function expandUrl() {
+  const urlToExpand = url + '?shortUrl=' + $inputField.val() + '&key=' + apiKey;
+  try {
+  	let response = await fetch(urlToExpand);
     if(response.ok) {
-      return response.json();
+      let jsonResponse = await response.json();
+      $responseField.append('<p> Your expanded URL is </p><p>' + jsonResponse.longUrl+ '</p>');
+return jsonResponse;
     }
-    throw new Error('Request failed!');
-  },
-  networkError => console.log(networkError.message)
-  ).then(jsonResponse => {
-  $responseField.append('<p> Your shortened URL is </p><p>' + jsonResponse.id + '</p>');
-return jsonResponse;});
-};
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
+// Clear page and call AJAX functions
 function expand() {
-	$responseField.empty();
-	expandUrl();
-	return false;
+  $responseField.empty();
+  expandUrl();
+  return false;
 };
 
 function shorten() {
-	$responseField.empty();
-	shortenUrl();
-	return false;
+  $responseField.empty();
+  shortenUrl();
+  return false;
 };
 
 $expandButton.click(expand);
